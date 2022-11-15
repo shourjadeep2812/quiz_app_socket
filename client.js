5bc55a2f91b7b6e6f53382d3e1d537e8bbae1f74
Shourjadeep Datta <shourjadeepdatta@gmail.com>
2022-11-15 16:40:43 +0530

    updates

--- client.js
+++ client.js
@@ -3,12 +3,17 @@ const socket = io.connect("http://127.0.0.1:3000/quiz")
 
 socket.emit("First_message","Hey server!!")
 
+let clientCount = 0
 
-
-socket.on("Questions",(ans)=>{
-    console.log(ans)
+socket.on("Questions",(ques)=>{
+    clientCount++
+    console.log(ques)
     socket.emit("answer","answer to the question")
-    socket.emit("winner","")
+    // if(clientCount == ques["id"])
+    // {
+    //     socket.emit("winner","")
+    // }
+    
 })
 
 
--- server.js
+++ server.js
@@ -10,8 +10,39 @@ const {Server} = require('socket.io')
 var currentTime1 = 0
 var currentTime2 = 0
 let clients = []
-let count = 0
+// let count = 0
 let answers = {}
+let questions = [
+  {
+    "id":1,
+    "question":"Python is what type of language?",
+    "optionA":"scripting",
+    "optionB":"compiling",
+    "optionC":"shell",
+    "optionD":"None",
+    "ans":"A"
+  },
+  {
+    "id":2,
+    "question":"What do we need to store huge amounts of data?",
+    "optionA":"File system",
+    "optionB":"Database system",
+    "optionC":"Cloud",
+    "optionD":"None",
+    "ans":"B"
+  },
+  {
+    "id":3,
+    "question":"What marks the end to a statement in programming?",
+    "optionA":",",
+    "optionB":":",
+    "optionC":";",
+    "optionD":".",
+    "ans":"C"
+  }
+]
+let questionObj = {}
+let index = 0
 const io = new Server(server,{
   cors:{
     // origin:"http://127.0.0.1:8000",
@@ -27,50 +58,64 @@ quiznamespace.on('connection',async (socket)=>{
     console.log(msg);
   })
   socket.join("room1")
+  // count++
+  // questionObj["count"] = count
+  // questionObj["ques"] = "ques"
 
 
   //  socket.broadcast.emit("Questions","ques")
-  setTimeout(()=>{
-    quiznamespace.to("room1").emit("Questions","ques")
-    currentTime1 = +new Date();
-    
-  },8000)
 
-    socket.on("answer",(answer)=>{
+  questions.forEach((ques,index)=>{
+    let interval = setInterval(()=>{
 
-        console.log("Inside answer")
-        currentTime2 = +new Date()
-        var timeTaken = currentTime2 - currentTime1
-        clients.push(socket.id)
-        answers[timeTaken] = {
-          "timeTaken": timeTaken,
-          "answer": answer,
-          "clientId":socket.id
-        }
-        clients.push(timeTaken)
-        console.log(answers)
-
-        
-      
-    })
-
-    socket.on("winner",(dummy)=>{
-      setTimeout(()=>{
-        let winningTime = Math.min(...clients)
-        let winner = answers[winningTime]
-        quiznamespace.to("room1").emit("winnerIs",winner)
-      },5000)
-      
-    })
-
-      
-
-    // quiznamespace.to("room1").emit("winnerIs",winnerId)
-    
-    
-
-
+      quiznamespace.to("room1").emit("Questions",ques)
+      currentTime1 = +new Date();
+      socket.on("answer",(answer)=>{
   
+        // console.log("Inside answer")
+          currentTime2 = +new Date()
+          var timeTaken = currentTime2 - currentTime1
+          clients.push(socket.id)
+          answers[timeTaken] = {
+            "timeTaken": timeTaken,
+            "answer": answer,
+            "clientId":socket.id
+          }
+          clients.push(timeTaken)
+          console.log(answers,questions[index])
+    })
+          
+          // if(index == questions.length)
+          // {
+          //   clearInterval(interval)
+            
+          //   let winningTime = Math.min(...clients)
+          //   let winner = answers[winningTime]
+          //   quiznamespace.to("room1").emit("winnerIs",winner)
+             
+          // }
+  
+    },5000*index)
+  })
+  
+
+      
+
+
+  //   socket.on("winner",(dummy)=>{
+
+  //     if(clients.length == count)
+  //     {
+  //       let winningTime = Math.min(...clients)
+  //       let winner = answers[winningTime]
+  //       quiznamespace.to("room1").emit("winnerIs",winner)
+  //     }   
+
+  //     // quiznamespace.to("room1").emit("winnerIs",winnerId)
+      
+  // })
+
+
 })
 
 
